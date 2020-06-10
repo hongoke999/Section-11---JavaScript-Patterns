03f8ab2c6388c772afc9d7e283510395c3bcb22c
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-10 14:54:57 +0300

    Singleton Pattern

--- app.js
+++ app.js
@@ -1,57 +1,23 @@
-//  Basic structure
+const Singleton = (function() {
+    let instance;
 
-// (function(){
-//     //  Declare private vars and functions
-
-//     return {
-//         //  Declare public var and functions
-//     }
-// });
-
-
-//  STANDARD MODULE PATTERN
-const UICtrl = (function(){
-    let text = 'Hello World';
-
-    const changeText = function() {
-        const element = document.querySelector('h1');
-        element.textContent = text;
+    function createInstance() {
+        const object = new Object({name: 'Brad'});
+        return object;
     }
 
     return {
-        callChangeText: function() {
-            changeText();
-            console.log(text);
+        getInstance: function() {
+            if(!instance) {
+                instance = createInstance();
+            }
+            return instance;
         }
     }
 })();
 
-// UICtrl.callChangeText();
-// UICtrl.changeText();
+const instanceA = Singleton.getInstance();
+const instanceB = Singleton.getInstance();
 
-// console.log(UICtrl.text);
-
-//  REVEALING MODULE PATTERN
-const ItemCtrl = (function(){
-    let data = [];
-
-    function add(item) {
-        data.push(item);
-        console.log('Item Added....');
-    }
-
-    function get(id) {
-        return data.find(item => {
-            return item.id === id
-        })
-    }
-
-    return {
-        add: add,
-        get: get
-    }
-})();
-
-ItemCtrl.add({ id: 1, name: 'John' });
-ItemCtrl.add({ id: 2, name: 'Mark' });
-console.log(ItemCtrl.get(2));
+console.log(instanceA === instanceB);
+console.log(instanceB);
