be2faaa14c0b2bba70a16a0dda4b847b2d79d426
Chaka Hongoke <hongoke999@yahoo.com>
2020-06-11 13:14:14 +0300

    Observer Pattern

--- appes6.js
+++ appes6.js
@@ -1,33 +1,36 @@
-function EventObserver() {
-    this.observers = [];
-}
+class EventObserver {
+    constructor() {
+        this.observers = [];
+    }
 
-EventObserver.prototype = {
-    subscribe: function(fn) {
+    subscribe(fn) {
         this.observers.push(fn);
         console.log(`You are now subscribed to ${fn.name}`);
-    },
-    unsubscribe: function(fn) {
+    }
+
+    unsubscribe(fn) {
         //  Filter out from the list whatever matches the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
-        this.observers = this.observers.filter(function(item) {
-            if(item !== fn) {
-                return item;
-            }            
+        this.observers = this.observers.filter(function (item) {
+        if (item !== fn) {
+            return item;
+        }
         });
         console.log(`You are now unsubscribed from ${fn.name}`);
-    },
-    fire: function() {
-        this.observers.forEach(function(item){
-            item.call();
+    }
+
+    fire() {
+        this.observers.forEach(function (item) {
+          item.call();
         });
     }
+  
 }
 
 const click = new EventObserver();
 
 //  Event Listeners
-document.querySelector('.sub-ms').addEventListener('click', function(){
-    click.subscribe(getCurMilliseconds);
+document.querySelector('.sub-ms').addEventListener('click', function () {
+  click.subscribe(getCurMilliseconds);
 });
 
 document.querySelector('.unsub-ms').addEventListener('click', function () {
@@ -47,9 +50,9 @@ document.querySelector('.fire').addEventListener('click', function () {
 });
 
 //  Click Handler
-const getCurMilliseconds = function() {
-    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
-}
+const getCurMilliseconds = function () {
+  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+};
 
 const getCurSeconds = function () {
   console.log(`Current Seconds: ${new Date().getSeconds()}`);
